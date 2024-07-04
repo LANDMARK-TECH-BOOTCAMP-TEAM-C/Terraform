da7b27767863328e442c9d056942e96e8412c25b
kemijones <kemijones09@gmail.com>
2024-07-04 09:34:04 -0400

    test

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 
-resource "aws_instance" "terraform_node" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  vpc_security_group_ids = [aws_security_group.terraform_node_sg.id]
-  subnet_id     = var.subnet_id
-  user_data     = file("${path.module}/userdata.sh")
+# resource "aws_instance" "terraform_node" {
+#   ami           = var.ami_id
+#   instance_type = var.instance_type
+#   key_name      = var.key_name
+#   vpc_security_group_ids = [aws_security_group.terraform_node_sg.id]
+#   subnet_id     = var.subnet_id
+#   user_data     = file("${path.module}/userdata.sh")
 
-  tags = {
-    Name = var.instance_tag
-  }
-}
+#   tags = {
+#     Name = var.instance_tag
+#   }
+# }
 
 resource "aws_security_group" "terraform_node_sg" {
   name        = var.sg_name
