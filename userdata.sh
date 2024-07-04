b511469d110a1e931d03ce8375a240e66750c0ba
kemijones <kemijones09@gmail.com>
2024-07-04 08:46:01 -0400

    test

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install -y unzip
+sudo apt install -y unzip curl
 sudo wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 sudo chmod +x /usr/bin/terraform
-terraform --version
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
-sudo ./aws/install -y
-aws --version
+sudo add-apt-repository universe -y
+sudo apt update -y
+sudo apt install snapd
+sudo snap install aws-cli --classic
