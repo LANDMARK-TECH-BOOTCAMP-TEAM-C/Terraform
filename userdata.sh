88b22a6a113b7aa98e7f9e0055942b679aa3404a
kemijones <kemijones09@gmail.com>
2024-07-04 07:47:28 -0400

    updating userdata

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt update
+sudo apt update -y
 sudo apt install -y unzip
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+sudo wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 sudo chmod +x /usr/bin/terraform
 terraform --version
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install -y
 aws --version
